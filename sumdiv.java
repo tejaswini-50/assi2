0e591c1f5709a045a6503a0127304399e84bf7ac
tejaswini-50 <121000631+tejaswini-50@users.noreply.github.com>
2024-03-13 18:04:56 +0530

    Create sumdiv.java

---
+++ sumdiv.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] values = {100, 52, 43, 99, 42, 40, 91};
+        
+        int sum = 0;
+        
+        for (int value : values) {
+            if (value % 5 == 0 && value % 2 == 0) {
+                sum += value;
+            }
+        }
+        
+        System.out.println("Sum of the numbers divisible by 5 and 2 are : " + sum);
+    }
+}
